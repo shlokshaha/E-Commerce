f9c14d6217238b2a0b2071d8519834030ea11571
shlokshaha <shlokshaha1421@gmail.com>
2021-09-26 17:37:39 +0530

    Payment Added

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#$e%yaqg=nsg(bwgl73_9n)mzf#1ucsv^r)=4gefgdo!23%v3f'
+SECRET_KEY = '#$e%yaqg=nsg(bwgl73_9n)mzf#1ucsv^r)=4gefgdo!23%v3f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
